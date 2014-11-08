a0ed508ab77bd59a4821cd0dbe61fd2319585be1
imarkwick <izzymarkwick@gmail.com>
2014-11-08 13:00:19 +0000

    Fizzbizz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,14 +1,14 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz' do 
+describe 'fizzbizz' do 
 
 	context 'knows when a number' do
 
-		it 'is divisible by three' do
+		it 'is divisible by three?' do
 			expect(is_divisible_by_three?(3)).to be true
 		end
 
-		it 'is divisible by five' do
+		it 'is divisible by five?' do
 			expect(is_divisible_by_five?(5)).to be true
 		end
 
@@ -16,15 +16,15 @@ describe 'fizzbuzz' do
 			expect(is_divisible_by_fifteen?(15)).to be true
 		end
 
-		it 'is NOT divisible by three' do
+		it 'is not divisible by three' do
 			expect(is_divisible_by_three?(1)).to be false
 		end
 
-		it 'is NOT divisible by five' do
-			expect(is_divisible_by_fifteen?(1)).to be false
+		it 'is not divisible by five' do
+			expect(is_divisible_by_five?(1)).to be false
 		end
 
-		it 'is NOT divisible by fifteen' do
+		it 'is not divisible by fifteen' do
 			expect(is_divisible_by_fifteen?(1)).to be false
 		end
 	end
@@ -32,16 +32,16 @@ end
 
 describe 'when playing fizzbuzz' do
 
-	it '"fizz" when divisible only by three' do
+	it '"fizz" when divisible by three' do
 		expect(fizzbuzz_says(3)).to eq "fizz"
 	end
 
-	it '"buzz" when divisible only by five' do
+	it '"bizz" when divisible by five' do
 		expect(fizzbuzz_says(5)).to eq "buzz"
 	end
 
-	it '"fizzbuzz" when divisible only by fifteen' do
-		expect(fizzbuzz_says(15)).to eq "fizzbuzz"
+	it '"fizzbizz" when divisible by fifteen' do
+		expect(fizzbuzz_says(15)).to eq "fizzbizz"
 	end
 
 end
