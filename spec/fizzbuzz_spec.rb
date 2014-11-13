cf8d67ed3423a458b0bd4e52465394ab98ce2206
imarkwick <izzymarkwick@gmail.com>
2014-11-13 08:57:46 +0000

    Fizzbuzz again again

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -16,35 +16,35 @@ describe 'fizzbizz' do
 			expect(is_divisible_by_fifteen?(15)).to be true
 		end
 
-		it 'doesn\'t fizz if not divisible by three?' do
+		it 'doesn\'t fizz if not divisible by three' do
 			expect(is_divisible_by_three?(1)).to be false
 		end
 
-		it 'doesn\'t buzz if not divisible by five?' do
+		it 'doesn\'t buzz if not divisible by five' do
 			expect(is_divisible_by_five?(1)).to be false
-		end	
+		end
 
-		it 'doesn\'t fizzbuzz if not divisible by fifteen?' do
+		it 'doesn\'t fizzbuzz if not divisible by fifteen' do
 			expect(is_divisible_by_fifteen?(1)).to be false
-		end	
+		end
 	end
 end
 
 describe 'when playing fizzbuzz' do
 
-	it '"fizz" when divisible by three' do
-		expect(fizzbuzz_says(3)).to eq "fizz"
+	it '"fizz" if divisible by only three' do
+		expect(fizzbuzz_says(3)).to eq "Fizz"
 	end
 
-	it '"buzz" when divisible by five' do
-		expect(fizzbuzz_says(5)).to eq "buzz"
+	it '"buzz" if divisible by only five' do
+		expect(fizzbuzz_says(5)).to eq "Buzz"
 	end
 
-	it '"fizzbuzz" when divisible by fifteen' do
-		expect(fizzbuzz_says(15)).to eq "fizzbuzz"
+	it '"fizzbuzz" if divisible by only fifteen' do
+		expect(fizzbuzz_says(15)).to eq "Fizzbuzz"
 	end
 
-	it 'returns the number if none of above' do
+	it 'returns the number if none of the above' do
 		expect(fizzbuzz_says(1)).to eq 1
 	end
 end
