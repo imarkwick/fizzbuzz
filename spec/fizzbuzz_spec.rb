f62e2f28ba2dc07468cd98fa7235e979b2fc9e79
imarkwick <izzymarkwick@gmail.com>
2014-11-14 08:56:46 +0000

    Fizzbuzzin while commutin

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -19,27 +19,34 @@ describe 'fizzbizz' do
 		it 'should not fizz if not divisible by three' do
 			expect(is_divisible_by_three?(1)).to be false
 		end
+
+		it 'should not buzz if not divisible by five' do
+			expect(is_divisible_by_five?(1)).to be false
+		end
+
+		it 'should not fizzbuzz if not divisible by fifteen' do
+			expect(is_divisible_by_fifteen?(1)).to be false
+		end
 	end
 end
 
 describe 'when playing fizzbuzz' do
 
-	it '"fizz" if divisible by three' do
+	it '"fizz" when divisible by three' do
 		expect(fizzbuzz_says(3)).to eq "Fizz"
 	end
 
-	it '"buzz" if divisible by five' do
+	it '"buzz" when divisible by five' do
 		expect(fizzbuzz_says(5)).to eq "Buzz"
 	end
 
-	it '"fizzbuzz" if divisible by fifteen' do
+	it '"fizzbuzz" when divisible by fifteen' do
 		expect(fizzbuzz_says(15)).to eq "Fizzbuzz"
 	end
 
 	it 'returns the number if none of the above' do
 		expect(fizzbuzz_says(1)).to eq 1
 	end
-
 end
 
 
