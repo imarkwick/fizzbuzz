bacb69770534e3038eb75aa495ce9aa56107701f
imarkwick <izzymarkwick@gmail.com>
2014-11-18 23:26:05 +0000

    tube fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,47 +4,47 @@ describe 'fizzbizz' do
 
 	context 'knows when a number' do
 
-		it 'should know when a number is divisible by three' do
-			expect(is_divisible_by_three?(3)).to be true
+		it 'should fizz if divisible by three' do
+			expect(is_divisible_by_three?(3)).to eq true
 		end
 
-		it 'should know when a number is divisible by five' do
-			expect(is_divisible_by_five?(5)).to be true
+		it 'should buzz if divisible by five' do
+			expect(is_divisible_by_five?(5)).to eq true
 		end
 
-		it 'should know when a number is divisible by fifteeen' do
-			expect(is_divisible_by_fifteen?(15)).to be true
+		it 'should fizzbuzz if divisible by fifteen' do
+			expect(is_divisible_by_fifteen?(15)).to eq true
 		end
 
 		it 'should not fizz if not divisible by three' do
-			expect(is_divisible_by_three?(1)).to be false
+			expect(is_divisible_by_three?(1)).to eq false
 		end
 
 		it 'should not buzz if not divisible by five' do
-			expect(is_divisible_by_five?(1)).to be false
+			expect(is_divisible_by_five?(1)).to eq false
 		end
 
 		it 'should not fizzbuzz if not divisible by fifteen' do
-			expect(is_divisible_by_fifteen?(1)).to be false
+			expect(is_divisible_by_fifteen?(1)).to eq false
 		end
 	end
 end
 
 describe 'when playing fizzbuzz' do
 
-	it 'should "fizz" if divisible by three' do
+	it '"fizz" when divisible by three' do
 		expect(fizzbuzz_says(3)).to eq "fizz"
 	end
 
-	it 'should "buzz" if divisible by five' do
+	it '"buzz" when divisible by five' do
 		expect(fizzbuzz_says(5)).to eq "buzz"
 	end
 
-	it 'should "fizzbuzz" if divisible by fifteen' do
+	it '"fizzbuzz" when divisible by fifteen' do
 		expect(fizzbuzz_says(15)).to eq "fizzbuzz"
 	end
 
-	it 'should return the number if none of above' do
+	it 'returns the number if none of the above' do
 		expect(fizzbuzz_says(1)).to eq 1
 	end
 end
