123e039171eb9d1d1f8da71a6cd37f1097f6f294
imarkwick <izzymarkwick@gmail.com>
2014-11-28 09:04:50 +0000

    again

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,51 +2,54 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
 
-	context 'knows when a number' do
+	context 'knows when a number is' do
 
-		it 'it is divisible by three' do
-			expect(is_divisible_by_three?(3)).to eq true
+		it 'divisible by three' do
+			expect(is_divisible_by_three?(3)).to be true
 		end
 
-		it 'is divisible by five' do
-			expect(is_divisible_by_five?(5)).to eq true
+		it'divisible by five' do
+			expect(is_divisible_by_five?(5)).to be true
 		end
 
-		it 'is divisible by fifteen' do
-			expect(is_divisible_by_fifteen?(15)).to eq true
+		it 'divisible by fifteen' do
+			expect(is_divisible_by_fifteen?(15)).to be true
 		end
-
 	end
 
 	context 'knows when a number is NOT' do
 
-		it 'is divisible by three' do
-			expect(is_divisible_by_three?(1)).to eq false
+		it 'divisible by three' do
+			expect(is_divisible_by_three?(1)).to be false
 		end
 
-		it 'is divisible by five' do
-			expect(is_divisible_by_five?(1)).to eq false
+		it 'divisible by five' do
+			expect(is_divisible_by_five?(1)).to be false
 		end
 
-		it 'is divisible by fifteen' do
-			expect(is_divisible_by_fifteen?(1)).to eq false
+		it 'divisible by fifteen' do
+			expect(is_divisible_by_fifteen?(1)).to be false
 		end
 	end
 
-	it '"Fizz" when divisible by three' do
-		expect(fizzbuzz_says(3)).to eq "Fizz"
-	end
+	context 'when playing, says' do
 
-	it '"Buzz" when divisible by five' do
-		expect(fizzbuzz_says(5)).to eq "Buzz"
-	end
+		it '"Fizz" when divisible by three' do
+			expect(fizzbuzz_says(3)).to eq "Fizz"
+		end
 
-	it '"Fizzbuzz when divisible by fifteen' do
-		expect(fizzbuzz_says(15)).to eq "Fizzbuzz"
-	end
+		it '"Buzz" when divisible by five' do
+			expect(fizzbuzz_says(5)).to eq "Buzz"
+		end
 
-	it 'returns the number if none of the above' do
-		expect(fizzbuzz_says(1)).to eq 1
-	end
+		it '"Fizzbuzz" when divisible by fifteen' do
+			expect(fizzbuzz_says(15)).to eq "Fizzbuzz"
+		end
 
+		it 'returns the number if none of the above' do
+			expect(fizzbuzz_says(1)).to eq 1
+		end	
+	end
 end
+
+
