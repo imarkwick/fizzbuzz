541cf67d70a763bf158cdcfe19e3b1c09890b237
imarkwick <izzymarkwick@gmail.com>
2014-12-11 10:00:44 +0000

    fizzbuzz for fun

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,7 +8,7 @@ describe 'fizzbuzz' do
 			expect(is_divisible_by_three?(3)).to be true
 		end
 
-		it'divisible by five' do
+		it 'divisible by five' do
 			expect(is_divisible_by_five?(5)).to be true
 		end
 
@@ -48,8 +48,9 @@ describe 'fizzbuzz' do
 
 		it 'returns the number if none of the above' do
 			expect(fizzbuzz_says(1)).to eq 1
-		end	
-	end
-end
+		end
 
+	end
+
+end
 
